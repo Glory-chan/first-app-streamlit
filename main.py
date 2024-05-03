27c76195ae10ea8b74ea3690171c1783996d3081
Glory-chan <kgodglory@gmail.com>
2024-05-03 16:23:32 +0100

    third commit

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ coefficients = np.polyfit(X_train.flatten(), y_train.flatten(), 2)
 y_pred = np.polyval(coefficients, X_train.flatten())
 r2 = r2_score(y_train, y_pred)
 # Définir la page Streamlit
+st.title('Kondo Godglory Ewoènam')
 st.title('Déploiement de modèle avec Streamlit')
 # Widget pour saisir une valeur d'entrée
 input_value = st.slider('Valeur d\'entrée', min_value=0.0, max_value=10.0, step=0.1)
